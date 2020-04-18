294d46f782e3ecf4dbee1a4cccef3882360e7c8c
Erik-FN <erik.fnoble@gmail.com>
2020-04-18 09:53:18 -0300

    finished proyect

--- btre/settings.py
+++ btre/settings.py
@@ -146,3 +146,10 @@ from django.contrib.messages import constants as messages
 MESSAGE_TAGS = {
     messages.ERROR: 'danger',
 }
+
+#Email Settings
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'erikdevportfolio@gmail.com'
+EMAIL_HOST_PASSWORD = 'laesclerosisfueinesperada@04'
+EMAIL_USE_TLS = True
--- contacts/views.py
+++ contacts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,redirect
 from .models import Contact
 from django.contrib import messages
+from django.core.mail import send_mail
 
 # Create your views here.
 
@@ -15,7 +16,27 @@ def contact(request):
         user_id = request.POST['user_id']
         realtor_email = request.POST['realtor_email']
 
+        #Check if the user made an inquiry for that propery already
+        if request.user.is_authenticated:
+            user_id = request.user.id
+            has_contacted = Contact.objects.all().filter(listing_id = listing_id, user_id = user_id)
+
+            if has_contacted:
+
+                messages.error(request, 'You already have an inquiry for this propery, wait for a response before making a new one')
+                return redirect ('/listings/'+listing_id)
+            
         contact = Contact(listing=listing, listing_id=listing_id, name=name, email=email, realtor_email=realtor_email,user_id=user_id,message=message, phone=phone)
         contact.save()
+
+        send_mail(
+            'Property Lising inquiry',
+            'There has been an inquiry for '+listing+' Sign into the admin panel to look at the contact',
+            'erikdevportfolio@gmail.com',
+            [realtor_email, 'nomadshae@gmail.com'],
+            fail_silently=False
+        )
+        
         messages.success(request, 'Request sumbited')
+        
         return redirect('/listings/'+listing_id)
